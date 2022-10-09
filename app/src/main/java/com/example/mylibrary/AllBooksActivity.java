b41eeb4aa1da5fd7a333c95c16f87768ebf649aa
heflinga <auston.hefling@outlook.com>
2022-10-09 19:45:27 -0400

    Created a cardview for all books, and added functionality pertaining to that.

--- app/src/main/java/com/example/mylibrary/AllBooksActivity.java
+++ app/src/main/java/com/example/mylibrary/AllBooksActivity.java
@@ -1,14 +1,32 @@
 package com.example.mylibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class AllBooksActivity extends AppCompatActivity {
+    private RecyclerView booksRecyclerView;
+    private BookRecViewAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_books);
+
+        adapter = new BookRecViewAdapter(this);
+        booksRecyclerView = findViewById(R.id.booksRecyclerView);
+
+        booksRecyclerView.setAdapter(adapter);
+        booksRecyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+
+        // Dummy array list
+        ArrayList<Book> books = new ArrayList<>();
+        books.add(new Book(1, "1Q84", "Haruki Murakami", 1350, "https://jameskennedymonash.files.wordpress.com/2013/01/1q84-cover.jpg",
+                "A work of maddening brilliance.", "Long description."));
+        adapter.setBooks(books);
     }
 }
