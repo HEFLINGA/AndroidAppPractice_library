cfdb62fab5ee68bc5b690e0d1a6e094494c5f930
heflinga <auston.hefling@outlook.com>
2022-10-22 08:36:28 -0400

    Created expandable graphic for books in list_item_book

--- app/src/main/java/com/example/mylibrary/AllBooksActivity.java
+++ app/src/main/java/com/example/mylibrary/AllBooksActivity.java
@@ -2,6 +2,7 @@ package com.example.mylibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
@@ -21,7 +22,7 @@ public class AllBooksActivity extends AppCompatActivity {
         booksRecyclerView = findViewById(R.id.booksRecyclerView);
 
         booksRecyclerView.setAdapter(adapter);
-        booksRecyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+        booksRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         // Dummy array list
         ArrayList<Book> books = new ArrayList<>();
